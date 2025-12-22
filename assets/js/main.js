940a5cbb63fca0b7c28f7ef60bcd02ff29557b4b
gnerella <nerellagoutham5@gmail.com>
2025-12-22 01:24:38 -0500

    Added menu options

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*================Menu Show Y Hidden================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*=====Menu Show=====*/
+/*Validate if constant exists*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*=====Menu Hidden=====*/
+/*Validate if constant exists*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
